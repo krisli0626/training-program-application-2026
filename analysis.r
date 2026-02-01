4361df292f95c3ec84658c9dfa3567215024407f
krisli0626 <krisli0626@gmail.com>
2026-02-01 14:04:26 +1100

    Complete analysis.R for exploratory RNA-seq analysis

--- analysis.r
+++ analysis.r
@@ -1,52 +1,92 @@
-# ---------------------------------------------------------
 
-# Melbourne Bioinformatics Training Program
+library(tidyverse)
 
-# This exercise to assess your familiarity with R and git. Please follow
-# the instructions on the README page and link to your repo in your application.
-# If you do not link to your repo, your application will be automatically denied.
+find_file <- function(fname, search_paths = c(".", "data", "Data")) {
+  for (p in search_paths) {
+    candidate <- file.path(p, fname)
+    if (file.exists(candidate)) return(candidate)
+  }
+  
 
-# Leave all code you used in this R script with comments as appropriate.
-# Let us know if you have any questions!
+  stop(
+    paste0(
+      "Could not find file: ", fname,
+      "\nSearched in: ", paste(search_paths, collapse = ", "),
+      "\nFiles in current directory: ", paste(list.files("."), collapse = ", ")
+    )
+  )
+}
 
+expr_fname <- "GSE60450_GeneLevel_Normalized(CPM.and.TMM)_data.csv"
+meta_fname <- "GSE60450_filtered_metadata.csv"
 
-# You can use the resources available on our training website for help:
-# Intro to R: https://mbite.org/intro-to-r
-# Version Control with Git: https://mbite.org/intro-to-git/
+expr_path <- find_file(expr_fname)
+meta_path <- find_file(meta_fname)
 
-# ----------------------------------------------------------
+expr <- read_csv(expr_path, show_col_types = FALSE)
+meta <- read_csv(meta_path, show_col_types = FALSE)
 
-# Load libraries -------------------
-# You may use base R or tidyverse for this exercise
+dim(expr)
+head(expr)
 
-# ex. library(tidyverse)
+dim(meta)
+head(meta)
 
-# Load data here ----------------------
-# Load each file with a meaningful variable name.
+meta2 <- meta %>%
+  rename(sample_id = 1)
 
+expr_long <- expr %>%
+  rename(ensembl_id = 1) %>%
+  pivot_longer(
+    cols = starts_with("GSM"),
+    names_to = "sample_id",
+    values_to = "expression"
+  )
 
+dat <- expr_long %>%
+  left_join(meta2, by = "sample_id")
 
-# Inspect the data -------------------------
+n_distinct(dat$sample_id)
 
-# What are the dimensions of each data set? (How many rows/columns in each?)
-# Keep the code here for each file.
+if (all(is.na(dat$immunophenotype))) {
+  stop("Join failed: immunophenotype is NA for all rows. Check sample_id matching between expression and metadata.")
+}
 
-## Expression data
+dat %>% count(immunophenotype, sort = TRUE)
 
+gene_summary <- dat %>%
+  group_by(ensembl_id, gene_symbol) %>%
+  summarise(mean_expression = mean(expression, na.rm = TRUE), .groups = "drop") %>%
+  arrange(desc(mean_expression))
 
-## Metadata
+top_gene <- gene_summary %>% slice(1)
 
+top_gene_id <- top_gene$ensembl_id[1]
+top_gene_symbol <- top_gene$gene_symbol[1]
 
-# Prepare/combine the data for plotting ------------------------
-# How can you combine this data into one data.frame?
+message("Selected gene for plotting: ", top_gene_symbol, " (", top_gene_id, ")")
 
+dat_one_gene <- dat %>%
+  filter(ensembl_id == top_gene_id)
 
+p <- ggplot(dat_one_gene, aes(x = immunophenotype, y = expression)) +
+  geom_boxplot() +
+  labs(
+    title = paste0("Expression by cell type: ", top_gene_symbol, " (", top_gene_id, ")"),
+    x = "Cell type (immunophenotype)",
+    y = "Expression (CPM / TMM)"
+  ) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 30, hjust = 1))
 
-# Plot the data --------------------------
-## Plot the expression by cell type
-## Can use boxplot() or geom_boxplot() in ggplot2
+print(p)
 
+if (!dir.exists("results")) dir.create("results")
 
-
-## Save the plot
-### Show code for saving the plot with ggsave() or a similar function
+ggsave(
+  filename = "results/expression_by_cell_type.png",
+  plot = p,
+  width = 9,
+  height = 5,
+  dpi = 300
+)
